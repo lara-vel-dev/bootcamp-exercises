41a0e87efc9ba34175582d1a13dd5510525abaad
Daniela Lara <laradni@icloud.com>
2022-11-26 18:24:33 -0600

    (Add) Card component

---
+++ 01-card/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import Greetings from "./Greetings"; 
+import "../css/card.css"
+
+function Card() {
+    return (<div className="card">
+        <Greetings className></Greetings>
+        <img className="profile-pic" src="src/img/profile.jpg" alt="Profile picture" />
+        <Greetings></Greetings>
+        <br />
+        <h3>I'm learning React!! :D</h3>
+    </div>);
+}
+
+export default Card;
---
+++ 01-card/src/components/Greetings.jsx
@@ -0,0 +1,8 @@
+function Greetings() {
+    let name = "Daniela";
+    return (<>
+        <h2>Hello! My name is {name}.</h2>
+    </>)
+}
+
+export default Greetings;
