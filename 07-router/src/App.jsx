0a197ff9acc446034c33a50a0e64fe4b5fe74ccd
Daniela Lara <laradni@icloud.com>
2023-01-09 18:36:18 -0600

    (Add) Exercise 07

---
+++ 07-router/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+  );
+};
+
+export default App;
